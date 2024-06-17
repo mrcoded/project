209b6b6114627baf215f0b4dd3fd52a224c8cab6
taiwo99 <taiwolateefat99@gmail.com>
2024-06-17 21:59:01 +0100

    commit

---
+++ Taiwo_workspace/script.js
@@ -0,0 +1,15 @@
+
+// const cardComponent = document.querySelector('.move-list')
+// console.log(cardComponent);
+// document.querySelector('.fa-search').addEventListener('click', function(){
+//     const getValue = document.getElementsByTagNam('input')
+//     getValue.textContent = 'hgjyuh,'
+//     console.log(getValue);
+// })
+
+
+
+//creat an array that saves all the movie compnent called allmovies
+//  if the search input found a match === to any allmovies child
+//the movie should be display first, then the other should follow
+// if not no found match
