694a9f9a47bac017dbeba02636c45aa479c2bdd1
taiwo99 <taiwolateefat99@gmail.com>
2024-06-18 20:24:59 +0100

    commit

--- Taiwo_workspace/script.js
+++ Taiwo_workspace/script.js
@@ -1,13 +1,39 @@
 
-// const cardComponent = document.querySelector('.move-list')
-// console.log(cardComponent);
-// document.querySelector('.fa-search').addEventListener('click', function(){
-//     const getValue = document.getElementsByTagNam('input')
-//     getValue.textContent = 'hgjyuh,'
-//     console.log(getValue);
-// })
+/*
+const movies = [
+    {title: 'Stranger Things', year: '2016', country: 'usa' },
+    {title: 'Batman Begins', year: '2005', country: 'usa' },
+    {title: 'Spider-man: Into The Spider verse', year: '2018', country: 'usa' },
+    {title: 'Dunkirk', year: '2017', country: 'usa' }
+]
+
+console.log(movies);
+
+// function to search and print the details
+
+function searchMovie(title){
+    const foundMovies = movies.filter(movie =>
+         movie.title.toLowerCase() === title.toLowerCase());
+
+         if (foundMovies.length > 0){
+            foundMovies.forEach(movie =>{
+                console.log(`movie: ${movie.title}`);
+                console.log(`year: ${movie.year}`);
+                console.log(`country: ${movie.country}`);
+                // sepertion btw movies
+                console.log('----');
+            
+
+            });
+         } else{
+            console.log(`no movie found with title ${title}`);
+         }
+}
+const inpuValue = 'stranger things'
+searchMovie(inpuValue)
 
 
+*/
 
 //creat an array that saves all the movie compnent called allmovies
 //  if the search input found a match === to any allmovies child
