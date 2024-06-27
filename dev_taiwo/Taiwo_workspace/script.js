d72fb170afe9920be26525edc13a5f27a28c5042
Taiwo lateefat <taiwolateefat99@gmail.com>
2024-06-27 21:34:23 +0100

    to level up the code

--- dev_taiwo/Taiwo_workspace/script.js
+++ dev_taiwo/Taiwo_workspace/script.js
@@ -1,7 +1,7 @@
 
 
  const getBtnValue = document.querySelector('button')
-const form = document.querySelector('.input')
+const input = document.querySelector('.input')
 const resultHeading = document.querySelector('.resultHeading')
 const movieResult = document.querySelector('.movie_result')
 let movieImg = document.querySelector('#result_movie_img')
@@ -9,180 +9,127 @@ let movieTitle = document.querySelector('.result_movie_title')
 let movieID = document.querySelector('.result_movie_id')
 let movieRelease = document.querySelector('.result_movie_releaseDate')
 let movieOverview = document.querySelector('.result_movie_overview')
+let movieTrailer = document.querySelector('.moviePlay')
+let playBtn = document.querySelector('.btn-toplay')
 
-//https://github.com/mrcoded/project
- 
-//const output = form.value;
-
- const apiKey = '1834f81129b73b73383a021cd0725af4'
- const poster = 'https://image.tmdb.org/t/p/original'
-
-//  async function searchMovie(query){
-         
-//      try {
-
-//        const response =  await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`);
-//         console.log(response);
-//         const data = await response.json();
-//         console.log(data);
-//              movieResult.innerHTML = '';
-//              movies.forEach(movie => {
-//                 const {poster_path, id, overview, title} = movie
-                 
-            
-//              });
-
-       
-
-       
-
-//     }catch(err){
-//         console.log(err);
-//     }
-
-//  }      
-//  searchMovie('home')
- //https://api.themoviedb.org/3/search/movie?query=home&api_key=1834f81129b73b73383a021cd0725af4
-// pMovie = fetch(`https://image.tmdb.org/t/p/original${result.poster_path}`)
 
 
 function searchMovie(e){
     e.preventDefault()
-    const query = form.value
+
+     const apiKey = '1834f81129b73b73383a021cd0725af4'
+     const maxSearch = 8;
+ 
+     // to get the search value
+    const query = input.value
 
     if (query.trim()){
 
         fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`).then((res) =>
             res.json())
+        
         .then((data) => {
             console.log(data);
             resultHeading.innerHTML = `search result for ${query}`;
 
-            // if condition is true
+            
             if(data.results === null){
                 resultHeading.innerHTML = `Oops! no result for ${query}`
                 
             }else{
-               movieResult.innerHTML = data.results.map((result) =>
-                  
-                `<div id="details">
-                <img src="https://image.tmdb.org/t/p/original${result.poster_path}" id="result_movie_img">
-               <div class="result_movie_title">
-                 ${result.title}
-               </div>
-               <div class="result_movie_id"> ${result.id}</div>
-                <div class="result_movie_releaseDate"> ${result.release_date}</div>
-               <div class="result_movie_overview"> ${result.overview}</div>
-              </div>
-                `
-               )
-             .join()
+                movieResult.innerHTML = data.results.map((result) => 
+
+                    `  <div class="result-container">
+                        <img src="https://image.tmdb.org/t/p/original${result.poster_path}" id="result_movie_img">
+                        <div class="result_movie_releaseDate"> ${result.release_date}</div>
+                        <div class="result_movie_title"><h4>${result.title}</h4></div>
+                        <div class="result_movie_id"> ${result.id}</div>
+                    </div>`
+                    
+                 )
+   
             }
-        });
-        //clear input field
-        form.value = '';
-    }else{
-        console.log('please enter movie to search for');
-    }
+            }).catch(error => {
+                    console.error('error',error);
+                })
+                input.value = '';
+            }else{
+                console.log('please enter movie to search for');
+            }
 }
 
-// function tohandleevent(){
-//     const query = document.querySelector('.input').value;
-//     searchMovie(query)
-
-// }
  getBtnValue.addEventListener('click', searchMovie)
   
 
-//,mn
 
-
-// fetch('https://api.themoviedb.org/3/search/movie?query=home%20alone&api_key=1834f81129b73b73383a021cd0725af4').then((data) => {
-
-//         console.log(data);
-//         return data.json();
-//     }).then((completedata) => {
-//         console.log(completedata);
-//         let data1 = "";
-//         completedata.map((values) =>{
-//             data1 = `<div class="card">
-//                         <h1 class="title">${values} </h1>
-//                         <p class="category">${values.category}</p>
-//                         <p class="price">${values.price}</p>
-//                     </div>`
-    
-//                 })
-// }).catch((err) =>{
-//     console.log(err);
-// })
-    
-
-  
-// document.getElementById("card").innerHTML = data1
-
-// https://api.themoviedb.org/3/movie/{movie_id}
-//https://developer.themoviedb.org/reference/intro/getting-started
-/*
-const movies = [
-    {title: 'Stranger Things', year: '2016', country: 'usa' },
-    {title: 'Batman Begins', year: '2005', country: 'usa' },
-    {title: 'Spider-man: Into The Spider verse', year: '2018', country: 'usa' },
-    {title: 'Dunkirk', year: '2017', country: 'usa' }
-]
-
-console.log(movies);
-
-// function to search and print the details
-
-function searchMovie(title){
-    const foundMovies = movies.filter(movie =>
-         movie.title.toLowerCase() === title.toLowerCase());
-
-         if (foundMovies.length > 0){
-            foundMovies.forEach(movie =>{
-                console.log(`movie: ${movie.title}`);
-                console.log(`year: ${movie.year}`);
-                console.log(`country: ${movie.country}`);
-                // sepertion btw movies
-                console.log('----');
-            
-
-            });
-         } else{
-            console.log(`no movie found with title ${title}`);
-         }
-}
-const inpuValue = 'stranger things'
-searchMovie(inpuValue)
-
-
-*/
-
-//creat an array that saves all the movie compnent called allmovies
-//  if the search input found a match === to any allmovies child
-//the movie should be display first, then the other should follow
-// if not no found match
-
-
-
-// async function AllmovieData(){
-//     try{
-//        const response = await fetch('jhgfdsdfghj')
-//        console.log(response);//geting the movie details
-
-//        const movieData = response.json
-//        console.log(movieData);
-
-//       }
+function playById(e){
+    e.preventDefault()
  
-//     catch(error){
-//      console.log('abc does not exist');
-//     }
-//     finally{
-//      console.log('code run anyways ');
-//     }
-     
-//  
- //const apiUrl ='https://api.themoviedb.org/3/search/movie?query=${}r&api_key=API_KEY'
+    togetid = '453026'
+    const apiKey = '1834f81129b73b73383a021cd0725af4'
+ 
+     fetch(`https://api.themoviedb.org/3/movie/${togetid}?&append_to_response=videos&api_key=${apiKey}`).then((response) =>
+       response.json())
+      //console.log(response);
+     .then((data) => {
+        console.log(data);
 
- //fetch(`https://api.themoviedb.org/3/search/movie?query=${output}r&api_key=${apiKey}`)
+      movieTitle = data.title
+      console.log(data.title);
+      movieID.innerHTML = data.id
+      movieRelease.innerHTML =  data.release_date
+      movieOverview.innerHTML =  data.overview
+      movieImg.innerHTML =  data.poster_path
+
+                      
+    
+    }).catch(error => {
+        console.error('Error fetching movie details:', error);
+    })  
+    
+          
+}
+
+
+playBtn.addEventListener('click', playFromTmbd)
+
+//playFromTmbd (' 343611', '1834f81129b73b73383a021cd0725af4')
+async function playFromTmbd(e) {
+    e.preventDefault();
+        const  movie_id = '343611'
+        const  api_key = '1834f81129b73b73383a021cd0725af4'
+    try{
+
+        const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${api_key}&append_to_response=videos`
+
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+        const trailerkey = data?.videos?.results?.[0]?.key;
+
+        if (trailerkey){
+            window.open(`https://www.youtube.com/watch?v=${trailerkey}`, '_blank')
+        }else{
+            console.log('NO trailer found for this movie');
+        }
+
+    }catch(error){
+        console.log('error fetching data');
+    }
+   
+}
+
+
+
+
+
+
+const movieList = document.querySelector('.movie-list');
+const mainViewToPlay = document.querySelector('.main-view')
+
+movieList.addEventListener('click', function() {
+
+    mainViewToPlay.style.display = 'block'
+
+})
