1ca6ec542514a29d2d30c2226c4c397e0f48c7d2
Taiwo lateefat <taiwolateefat99@gmail.com>
2024-06-29 21:36:20 +0100

    rectifying some changes

--- dev_taiwo/script.js
+++ dev_taiwo/script.js
@@ -3,7 +3,7 @@
  const getBtnValue = document.querySelector('button')
 const input = document.querySelector('.input')
 const resultHeading = document.querySelector('.resultHeading')
-const movieResult = document.querySelector('.movie_result')
+const movieResult = document.querySelector('.all-search-result')
 let movieImg = document.querySelector('#result_movie_img')
 let movieTitle = document.querySelector('.result_movie_title')
 let movieID = document.querySelector('.result_movie_id')
@@ -11,6 +11,9 @@ let movieRelease = document.querySelector('.result_movie_releaseDate')
 let movieOverview = document.querySelector('.result_movie_overview')
 let movieTrailer = document.querySelector('.moviePlay')
 let playBtn = document.querySelector('.btn-toplay')
+const moviecontainer = document.querySelector('.result.container');
+const mainViewToPlay = document.querySelector('.main-view')
+
 
 
 
@@ -38,16 +41,17 @@ function searchMovie(e){
                 
             }else{
                 movieResult.innerHTML = data.results.map((result) => 
-
-                    `  <div class="result-container">
+           
+                      
+                   `  <div class="movie">
                         <img src="https://image.tmdb.org/t/p/original${result.poster_path}" id="result_movie_img">
                         <div class="result_movie_releaseDate"> ${result.release_date}</div>
-                        <div class="result_movie_title"><h4>${result.title}</h4></div>
                         <div class="result_movie_id"> ${result.id}</div>
+                        <div class="result_movie_title"><h4>${result.title}</h4></div>
                     </div>`
                     
                  )
-   
+               .join()                   
             }
             }).catch(error => {
                     console.error('error',error);
@@ -58,39 +62,8 @@ function searchMovie(e){
             }
 }
 
- getBtnValue.addEventListener('click', searchMovie)
+ //getBtnValue.addEventListener('click', searchMovie)
   
-//what
-
-function playById(e){
-    e.preventDefault()
- 
-    togetid = '453026'
-    const apiKey = '1834f81129b73b73383a021cd0725af4'
- 
-     fetch(`https://api.themoviedb.org/3/movie/${togetid}?&append_to_response=videos&api_key=${apiKey}`).then((response) =>
-       response.json())
-      //console.log(response);
-     .then((data) => {
-        console.log(data);
-
-      movieTitle = data.title
-      console.log(data.title);
-      movieID.innerHTML = data.id
-      movieRelease.innerHTML =  data.release_date
-      movieOverview.innerHTML =  data.overview
-      movieImg.innerHTML =  data.poster_path
-
-                      
-    
-    }).catch(error => {
-        console.error('Error fetching movie details:', error);
-    })  
-    
-          
-}
-
-
 playBtn.addEventListener('click', playFromTmbd)
 
 //playFromTmbd (' 343611', '1834f81129b73b73383a021cd0725af4')
@@ -121,15 +94,74 @@ async function playFromTmbd(e) {
 }
 
 
+function toGetInfo (e) {
+    e.preventDefault()
+    const query = input.value
+    const apiKey = '1834f81129b73b73383a021cd0725af4'
+    
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`).then((res) =>
+        res.json())
+    
+    .then((data) => {
+        console.log(data);
+        resultHeading.innerHTML = `search result for ${query}`;
+
+        
+        if(data.results === null){
+            resultHeading.innerHTML = `Oops! no result for ${query}`
+            
+        }else{
+            movieResult.innerHTML = data.results.map((result) => 
+       
+                  
+               `  <div class="movie" onclick="pickID()">
+                    <img src="https://image.tmdb.org/t/p/original${result.poster_path}" id="result_movie_img">
+                    <div class="result_movie_id"> ${result.id}</div>
+                     <div class="result_movie_releaseDate"> ${result.release_date}</div>
+                     <div class="result_movie_title"><h4>${result.title}</h4></div>
+                </div>`
+            
+               
+
+            )
+
+            input.value = '';
+            async function pickID(){
+
+             const  id = data.results[0].id
+             const api_key = '1834f81129b73b73383a021cd0725af4'
+             try{
+
+                const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&append_to_response=videos`
+        
+                const response = await fetch(url);
+                const data = await response.json();
+                console.log(data);
+        
+                const trailerkey = data?.videos?.results?.[0]?.key;
+        
+                if (trailerkey){
+                    window.open(`https://www.youtube.com/watch?v=${trailerkey}`, '_blank')
+                }else{
+                    console.log('NO trailer found for this movie');
+                }
+        
+            }catch(error){
+                console.log('error fetching data');
+            }
+             
+            }
+            pickID()
+            
+        }
+        }).catch(error => {
+                console.error('error',error);
+    })
+            
+        
 
 
+}
 
+getBtnValue.addEventListener('click', toGetInfo)
 
-const movieList = document.querySelector('.movie-list');
-const mainViewToPlay = document.querySelector('.main-view')
-
-// movieList.addEventListener('click', function() {
-
-//     mainViewToPlay.style.display = 'block'
-
-// })
